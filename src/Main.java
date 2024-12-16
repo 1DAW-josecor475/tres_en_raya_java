d800bc0bcce48bcdbb643740548e707a72a27e64
1DAW-manuelleo269 <mleorod0105@g.educaand.es>
2024-12-16 09:45:07 +0100

    Valid movement function

--- src/Main.java
+++ src/Main.java
@@ -13,4 +13,32 @@ public class Main {
             System.out.println("");
         }
     }
+
+    public static boolean validMovement (char board[][], int movement){
+        /* Función: comprueba si el movimiento que hizo el jugador es válido.
+
+        Args:
+        - char board[][]: para comprobar las casillas del tablero.
+        - int movement: para comprobar que se introduce un número que tenga una casilla asignada.
+
+        Devuelve "true" si el movimiento es válido.
+         */
+        switch (movement) {
+            case 1: return board[0][0] == '-';
+            case 2: return board[0][1] == '-';
+            case 3: return board[0][2] == '-';
+            case 4: return board[1][0] == '-';
+            case 5: return board[1][1] == '-';
+            case 6: return board[1][2] == '-';
+            case 7: return board[2][0] == '-';
+            case 8: return board[2][1] == '-';
+            case 9: return board[2][2] == '-';
+        }
+
+        if (movement > 0 || movement < 10) {
+            return true;
+        }
+        
+        return false;
+    }
 }
