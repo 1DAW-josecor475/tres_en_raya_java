3efa0892f642eccf6d7bff864f06fef81ababdca
1DAW-manuelleo269 <mleorod0105@g.educaand.es>
2024-12-16 09:11:50 +0100

    Print board function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    }
+    public static void printBoard() {
+        char[][] board = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+                System.out.print("-         ");
+            }
+            System.out.println("");
+        }
     }
 }
